085c6a41b66d477591fabc8d8c76ad7b4446e962
kaxzuma <44837694+kaxzuma@users.noreply.github.com>
2019-11-17 16:44:22 +0900

    Update g.bat

--- g.bat
+++ g.bat
@@ -1,4 +1,7 @@
 @echo off
+rem ###############################################
+rem ## Google検索する作業の効率化バッチ
+rem ###############################################
 set words=%1 %2 %3 %4 %5 %6 %7 %8 %9
 call :trim %words%
 start chrome "https://www.google.co.jp/search?q=%words%"
